748c3a99fc1003ce9dc4917961e3430dd37f4882
raylu <lurayl@gmail.com>
2017-07-29 22:56:00 -0700

    render months

--- processed/ddd.js
+++ processed/ddd.js
@@ -2,9 +2,26 @@ window.addEvent('domready', function() {
 	'use strict';
 
 	new Request.JSON({
+		'url': 'months.json',
+		'onSuccess': months,
+	}).get();
+
+	new Request.JSON({
 		'url': 'all_time.json',
 		'onSuccess': all_time,
 	}).get();
+
+	function months(data) {
+		const table = $('months').getElement('tbody');
+		data.each((month) => {
+			const row = new Element('tr');
+			row.adopt(
+				new Element('td', {'text': month['month']}),
+				new Element('td', {'text': month['count'].toLocaleString()}),
+			);
+			table.grab(row);
+		});
+	}
 
 	function all_time(data) {
 		const table = $('all_time').getElement('tbody');
