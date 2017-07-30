433fdfef33937fd90c28edb63783b63d87605c73
raylu <lurayl@gmail.com>
2017-07-30 15:03:29 -0700

    stats server, with hour breakdown

--- ddd.py
+++ ddd.py
@@ -1,79 +1,65 @@
 #!/usr/bin/env python3
 
-import csv
-from collections import Counter
-import json
-import lzma
-import os
-from os import path
-import sys
+import datetime
+import sqlite3
 
-def main():
-	messages_xz_path = sys.argv[1]
-	users_path = sys.argv[2]
+from pigwig import PigWig, Response
 
-	users = get_users(users_path)
-	all_time = process_all_time(users, messages_xz_path)
-	with open('processed/all_time.json', 'w') as f:
-		json.dump(all_time, f, indent='\t')
-	months = process_months()
-	with open('processed/months.json', 'w') as f:
-		json.dump(months, f, indent='\t')
+def root(request):
+	return Response.render(request, 'index.html', {})
 
-def process_all_time(users, messages_xz_path):
-	user_counts = Counter()
-	for row in iter_rows(messages_xz_path):
-		user_id = int(row['user_id'])
-		user_counts[user_id] += 1
+def months(request):
+	c = conn.cursor()
+	c.execute('''
+		SELECT strftime("%Y-%m", hour, "unixepoch") AS month, SUM(count)
+		FROM messages GROUP BY month ORDER BY month ASC
+	''')
+	data = []
+	for row in c.fetchall():
+		data.append({'month': row['month'], 'count': row['SUM(count)']})
+	return Response.json(data)
 
-	total = sum(user_counts.values())
-	all_time = []
-	for user_id, message_count in user_counts.most_common(50):
-		name = users.get(user_id, user_id)
-		percentage = float('%.2f' % (message_count / total * 100))
-		all_time.append({'user': name, 'messages': message_count, 'percentage': percentage})
+def hours(request):
+	c = conn.cursor()
+	c.execute('''
+		SELECT strftime('%H', hour, 'unixepoch') AS agg_hour, SUM(count)
+		FROM messages GROUP BY agg_hour ORDER BY agg_hour ASC;
+	''')
+	data = []
+	for row in c.fetchall():
+		data.append({'hour': row['agg_hour'], 'count': row['SUM(count)']})
+	return Response.json(data)
 
-	return all_time
+def all_time(request):
+	c = conn.cursor()
+	c.execute('SELECT SUM(count) FROM messages')
+	total = c.fetchall()[0]['SUM(count)']
+	c.execute('''
+		SELECT name, SUM(count) FROM messages
+		JOIN users ON messages.user_id = users.user_id
+		GROUP BY users.user_id ORDER BY SUM(count) DESC LIMIT 50
+	''')
+	data = []
+	for row in c.fetchall():
+		count = row['SUM(count)']
+		data.append({
+			'name': row['name'],
+			'count': count,
+			'percentage': float('%.2f' % (count / total * 100)),
+		})
+	return Response.json(data)
 
-def process_months():
-	month_counts = {}
-	for day_filename in sorted(os.listdir('logs')):
-		if not day_filename.endswith('.csv'):
-			raise Exception(day_filename)
-		month = day_filename[:7]
-		if month not in month_counts:
-			print('processing', month)
-			month_counts[month] = 0
+routes = [
+	('GET', '/', root),
+	('GET', '/months.json', months),
+	('GET', '/hours.json', hours),
+	('GET', '/all_time.json', all_time),
+]
 
-		lines = 0
-		with open(path.join('logs', day_filename), 'r') as f:
-			next(f) # skip header
-			for _ in f:
-				lines += 1
+app = PigWig(routes, template_dir='templates')
 
-		month_counts[month] += lines
-
-	months = []
-	for month, count in month_counts.items():
-		months.append({'month': month, 'count': count})
-	return months
-
-def get_users(users_path):
-	users = {}
-	with open(users_path, 'r') as f:
-		reader = csv.DictReader(f)
-		for row in reader:
-			user_id = int(row['user_id'])
-			users[user_id] = row['name']
-	return users
-
-def iter_rows(messages_xz_path):
-	with lzma.open(messages_xz_path, 'rt', encoding='utf-8') as f:
-		reader = csv.DictReader(f)
-		for i, row in enumerate(reader):
-			yield row
-			if (i + 1) % 10000 == 0:
-				print('processed', i+1, 'messages')
+conn = sqlite3.connect('ddd.db')
+conn.row_factory = sqlite3.Row
 
 if __name__ == '__main__':
-	main()
+	app.main()
--- static/ddd.js
+++ static/ddd.js
@@ -7,6 +7,11 @@ window.addEvent('domready', function() {
 	}).get();
 
 	new Request.JSON({
+		'url': 'hours.json',
+		'onSuccess': hours,
+	}).get();
+
+	new Request.JSON({
 		'url': 'all_time.json',
 		'onSuccess': all_time,
 	}).get();
@@ -30,6 +35,25 @@ window.addEvent('domready', function() {
 		});
 	}
 
+	function hours(data) {
+		const max = Math.max.apply(null, data.map((hour) => hour['count']));
+
+		const table = $('hours').getElement('tbody');
+		data.each((hour) => {
+			const row = new Element('tr');
+			const bar = new Element('div', {
+				'class': 'bar',
+				'styles': {'width': hour['count'] / max * 500},
+			});
+			row.adopt(
+				new Element('td', {'text': hour['hour']}),
+				new Element('td', {'text': hour['count'].toLocaleString(), 'class': 'right'}),
+				new Element('td').grab(bar),
+			);
+			table.grab(row);
+		});
+	}
+
 	function all_time(data) {
 		const table = $('all_time').getElement('tbody');
 		let cumulative = 0;
@@ -47,7 +71,7 @@ window.addEvent('domready', function() {
 			row.adopt(
 				new Element('td', {'text': i + 1}),
 				new Element('td', {'text': user['user']}),
-				new Element('td', {'text': user['messages'].toLocaleString(), 'class': 'right'}),
+				new Element('td', {'text': user['count'].toLocaleString(), 'class': 'right'}),
 				new Element('td', {'text': user['percentage'].toFixed(2), 'class': 'right'}),
 				new Element('td').adopt(filler, bar),
 				new Element('td', {'text': cumulative.toFixed(2), 'class': 'right'}),
