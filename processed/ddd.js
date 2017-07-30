789f7bee2f8e26ae84664a66fff02761cf9deca7
raylu <lurayl@gmail.com>
2017-07-29 23:16:49 -0700

    bar "graphs"

--- processed/ddd.js
+++ processed/ddd.js
@@ -12,12 +12,19 @@ window.addEvent('domready', function() {
 	}).get();
 
 	function months(data) {
+		const max = Math.max.apply(null, data.map((month) => month['count']));
+
 		const table = $('months').getElement('tbody');
 		data.each((month) => {
 			const row = new Element('tr');
+			const bar = new Element('div', {
+				'class': 'bar',
+				'styles': {'width': month['count'] / max * 500},
+			});
 			row.adopt(
 				new Element('td', {'text': month['month']}),
-				new Element('td', {'text': month['count'].toLocaleString()}),
+				new Element('td', {'text': month['count'].toLocaleString(), 'class': 'right'}),
+				new Element('td').grab(bar),
 			);
 			table.grab(row);
 		});
@@ -25,12 +32,25 @@ window.addEvent('domready', function() {
 
 	function all_time(data) {
 		const table = $('all_time').getElement('tbody');
-		data.each((user) => {
+		let cumulative = 0;
+		data.each((user, i) => {
 			const row = new Element('tr');
+			const filler = new Element('div', {
+				'class': 'bar filler',
+				'styles': {'width': cumulative * 5},
+			});
+			const bar = new Element('div', {
+				'class': 'bar',
+				'styles': {'width': user['percentage'] * 5},
+			});
+			cumulative += user['percentage'];
 			row.adopt(
+				new Element('td', {'text': i + 1}),
 				new Element('td', {'text': user['user']}),
-				new Element('td', {'text': user['messages'].toLocaleString()}),
-				new Element('td', {'text': user['percentage']}),
+				new Element('td', {'text': user['messages'].toLocaleString(), 'class': 'right'}),
+				new Element('td', {'text': user['percentage'].toFixed(2), 'class': 'right'}),
+				new Element('td').adopt(filler, bar),
+				new Element('td', {'text': cumulative.toFixed(2), 'class': 'right'}),
 			);
 			table.grab(row);
 		});
