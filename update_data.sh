06224390eab7969d7abfce473eda4d30791e54cb
raylu <lurayl@gmail.com>
2017-09-29 22:20:43 -0700

    update_data.sh

--- update_data.sh
+++ update_data.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+
+set -eux
+
 psql -h lilith.undo.it -U statbot_ro -d statbot -f dump_csvs.sql
 rm -f raw/messages.csv.lzma
 lzma raw/messages.csv
+
+./prepare_db.py
+#./markov.py gen
