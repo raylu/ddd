31a8409ec397cc4f218da79dec4e3013b0a886c6
raylu <lurayl@gmail.com>
2020-02-20 21:26:55 -0800

    new hostname for lilith

--- update_data.sh
+++ update_data.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-psql -h lilith.undo.it -U statbot_ro -d statbot -f dump_csvs.sql --quiet --echo-errors
+psql -h programming.im -U statbot_ro -d statbot -f dump_csvs.sql --quiet --echo-errors
 rm -f raw/messages.csv.lzma
 lzma raw/messages.csv
 
